db1edee4df1cdba940404b067319d91767de5755
sx1989827 <395414574@qq.com>
2015-12-26 11:45:59 +0800

    add

---
+++ SkyFighting/ViewController/MainTabVC.m
@@ -0,0 +1,44 @@
+//
+//  MainTabVC.m
+//  SkyFighting
+//
+//  Created by 孙昕 on 15/12/24.
+//  Copyright © 2015年 孙昕. All rights reserved.
+//
+
+#import "MainTabVC.h"
+
+@interface MainTabVC ()
+
+@end
+
+@implementation MainTabVC
+-(instancetype)init
+{
+    if(self=[super init])
+    {
+        
+    }
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
